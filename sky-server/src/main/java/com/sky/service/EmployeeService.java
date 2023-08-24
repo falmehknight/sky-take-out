f8917d221e5a8b05fa9d5134103b2db048a6548b
SA22225337 <tyh225337@mail.ustc.edu.cn>
2023-08-24 22:50:22 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.sky.service;
 
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 
@@ -8,8 +9,10 @@ public interface EmployeeService {
     /**
      * 员工登录
      * @param employeeLoginDTO
-     * @return
+     * @return Employee
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
+
+    void save(EmployeeDTO employeeDTO);
 }
