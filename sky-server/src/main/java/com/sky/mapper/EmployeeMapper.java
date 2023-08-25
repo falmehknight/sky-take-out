06a240802529303c72b7fdbae16c03c562c7a24b
SA22225337 <tyh225337@mail.ustc.edu.cn>
2023-08-25 23:32:18 +0800

    员工查询分页业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -15,10 +17,26 @@ public interface EmployeeMapper {
      */
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
-
+    /**
+     *
+     * @Author TanYingHao
+     * @Description 插入一个员工
+     * @Date 22:35 2023/8/25
+     * @Param [employee]
+     * @return void
+     **/
     @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, " +
             "create_user, update_user,status)" + "values" + "(#{name}, #{username}, #{password}, #{phone}, #{sex}, " +
             "#{idNumber},#{createTime}, #{updateTime}, #{createUser}, #{updateUser},#{status})"
             )
     void insert(Employee employee);
+    /**
+     *
+     * @Author TanYingHao
+     * @Description 分页查询
+     * @Date 22:36 2023/8/25
+     * @Param [employeePageQueryDTO]
+     * @return com.github.pagehelper.Page<com.sky.entity.Employee>
+     **/
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
